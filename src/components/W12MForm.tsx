9b0e07aaa96c3705c25a5b406d3cdc4f6a119a4f
TimalkaK <timalkamk@gmail.com>
2023-09-13 19:17:47 +0100

    feat: Implemented PlanetName component

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,16 +1,19 @@
 import { useState } from 'react';
 import W12MHeader from './W12MHeader';
 import { SpeciesName } from './speciesName/speciesName';
+import { PlanetName } from './planetName/planetName';
 
 const W12MForm = () => {
 
 	const [speciesName, setSpeciesName] = useState('humans');
+	const [planetName, setPlanetName] = useState('earth');
 
 
 	return (
 		<section className='w12MForm'>
 			<W12MHeader />
 			<SpeciesName speciesName={speciesName} onChangeSpeciesName={(value) => setSpeciesName(value)} />
+			<PlanetName planetName={planetName} onChangePlanetName={(value) => setPlanetName(value)} />
 		</section>
 	);
 };
---
+++ src/components/planetName/planetName.tsx
@@ -0,0 +1,10 @@
+interface PlanetNameProps {
+	planetName: string;
+	onChangePlanetName: (value: string) => void;
+}
+
+export const PlanetName : React.FC<PlanetNameProps> = ({speciesName: planetName, onChangePlanetName}) => (
+  <>
+      <label htmlFor='planetName'>Planet Name</label>
+      <input id='planetName' type='text' value={planetName} onChange={(e) => onChangePlanetName(e.target.value)} />
+  </> );
