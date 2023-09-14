7bb5aae7b0d51aed8263000f8b792413601ac9fb
TimalkaK <timalkamk@gmail.com>
2023-09-14 19:36:58 +0100

    feat: Implemented button for form

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -11,18 +11,29 @@ const W12MForm = () => {
 	const [speciesName, setSpeciesName] = useState('humans');
 	const [planetName, setPlanetName] = useState('earth');
 	const [numberOfBeings, setNumberOfBeings] = useState('7.8 billion');
-	const [selectedValue, setSelectedValue] = useState('');
-	const [reasonForSparing, setReasonForSparing] = useState('');
+	const [selectedValue, setSelectedValue] = useState('Not 4');
+	const [reasonForSparing, setReasonForSparing] = useState('We love our planet');
+
+	const handleClick = () => {
+		console.log (speciesName);
+		console.log (planetName);
+		console.log (numberOfBeings);
+		console.log (selectedValue);
+		console.log (reasonForSparing);
+	}
 
 
 	return (
 		<section className='w12MForm'>
 			<W12MHeader />
-			<SpeciesName speciesName={speciesName} onChangeSpeciesName={(value) => setSpeciesName(value)} />
-			<PlanetName planetName={planetName} onChangePlanetName={(value) => setPlanetName(value)} />
-			<NumberOfBeings numberOfBeings={numberOfBeings} onChangeNumberOfBeings={(value) => setNumberOfBeings(value)} />
-			<WhatIs2Plus2 selectedValue={selectedValue} onChangeSelectedValue={(value) => setSelectedValue(value)}/>
-			<ReasonForSparing reasonForSparing={reasonForSparing} onChangeReasonForSparing={(value) => setReasonForSparing(value)}/>
+			<ul>
+				<li><SpeciesName speciesName={speciesName} onChangeSpeciesName={(value) => setSpeciesName(value)} /></li>
+				<li><PlanetName planetName={planetName} onChangePlanetName={(value) => setPlanetName(value)} /></li>
+				<li><NumberOfBeings numberOfBeings={numberOfBeings} onChangeNumberOfBeings={(value) => setNumberOfBeings(value)} /></li>
+				<li><WhatIs2Plus2 selectedValue={selectedValue} onChangeSelectedValue={(value) => setSelectedValue(value)}/></li>
+				<li><ReasonForSparing reasonForSparing={reasonForSparing} onChangeReasonForSparing={(value) => setReasonForSparing(value)}/></li>
+			</ul>
+			<button onClick={handleClick}>Submit Form</button>
 		</section>
 	);
 };
